86eec6955547b65a15ba7eaa0f689f2a6b25542f
LesovoyRoman <romalesov@ukr.net>
2018-11-18 16:00:04 +0200

    -- React -- -> Added component 'Question' -> updated app rendering in App.js

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,16 +1,21 @@
 import React, { Component } from 'react';
+import {Route} from 'react-router-dom'
 import NavBar from './NavBar/NavBar'
 import Questions from './Questions/Questions';
+import Question from "./Questions/Question";
+const Path_port = 'http://localhost:7777/';
 
 class App extends Component {
-  render() {
-    return (
-      <div>
-          <NavBar/>
-          <Questions/>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div>
+                <NavBar/>
+                <Route exact path="/" component={Questions}/>
+                <Route exact path="/question/:questionId" component={Question} />
+            </div>
+        );
+    }
 }
 
 export default App;
+export {Path_port};
