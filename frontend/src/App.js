a0a70f349fa87f205a8fb167472a846fac87f55d
LesovoyRoman <romalesov@ukr.net>
2018-11-18 22:27:08 +0200

    -- React -- -> App.js => use Provider, store

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,7 +1,8 @@
 import React, { Component } from 'react';
 
-
-import {Route} from 'react-router-dom'
+import { BrowserRouter as Router, Route } from 'react-router-dom';
+import { Provider } from 'react-redux';
+import store from './store';
 
 import NavBar from './components/NavBar/NavBar'
 import Questions from './components/Questions/Questions';
@@ -10,21 +11,27 @@ import Register from "./components/Auth/Register"
 import Login from "./components/Auth/Login";
 
 
-
 class App extends Component {
     render() {
         return (
-            <div>
-                <NavBar/>
-                <Route exact path="/" component={Questions}/>
-                <Route exact path="/question/:questionId" component={Question} />
-                <Route exact path="/register" component={Register} />
-                <Route exact path="/login" component={Login} />
-            </div>
+            <Provider store={ store }>
+                <Router>
+                    <div>
+                        <NavBar/>
+                        <div className="container">
+                            <Route exact path="/" component={Questions}/>
+                            <Route exact path="/question/:questionId" component={Question} />
+                            <Route exact path="/register" component={Register} />
+                            <Route exact path="/login" component={Login} />
+                        </div>
+                    </div>
+                </Router>
+            </Provider>
         );
     }
 }
 
 const Path_port = 'http://localhost:7777/api/';
+
 export default App;
 export {Path_port};
