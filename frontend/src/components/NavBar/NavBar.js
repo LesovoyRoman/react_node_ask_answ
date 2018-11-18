80dba2e7e58415ebcf5b3fddd42b815954b28b44
LesovoyRoman <romalesov@ukr.net>
2018-11-18 22:02:07 +0200

    -- React -- -> added logo to NavBar

--- frontend/src/components/NavBar/NavBar.js
+++ frontend/src/components/NavBar/NavBar.js
@@ -1,10 +1,12 @@
 import React from 'react';
 import {Link} from 'react-router-dom';
+import logo from '../../logo.svg'
 
 function NavBar() {
     return (
         <nav className="navbar navbar-dark bg-primary fixed-top">
             <Link className="navbar-brand" to="/">
+                <img src={logo} className="App-logo" style={{ width: '40px' }} alt=""/>
                 Home
             </Link>
             <Link className="navbar-brand" to="/register">
